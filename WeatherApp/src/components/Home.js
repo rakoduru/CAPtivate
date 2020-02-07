a5952c90b3332219ce59ebd4d8f7563357f7e2a5
rasmitha-koduru <rasmithachoudary@gmail.com>
2020-02-07 00:48:45 -0500

    Bootstrap Basic UI using React (#18)

---
+++ WeatherApp/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const home = () => {
+    return (
+       <div>
+          <h1>Home</h1>
+           <p>Welcome to Captivate</p>
+       </div>
+    );
+}
+
+export default home;
