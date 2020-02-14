a4bb6deee6b9e2ad6fcb0710d9c03d13f49ba503
Adithya Selvaprithiviraj <adithyaselv@gmail.com>
2020-02-13 19:32:21 -0500

    Improve data retrieval service (#59)

--- data-retrieval/data-retrieval.py
+++ data-retrieval/data-retrieval.py
@@ -8,7 +8,7 @@ import copy
 
 mclient = MongoClient(port=27014)
 
-db = mclient.foo2
+db = mclient.weather_data
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x: 
@@ -23,31 +23,41 @@ headers = {'token':'tlOrjLlVkdmPriWTBeKteqdOURnHqvjL'}
 url ='https://www.ncdc.noaa.gov/cdo-web/api/v2/data'
 
 while True:
-    m = consumer.poll()
-    # params = {'datasetid': 'GHCND', 'locationid':'ZIP:28801', 'startdate': '2010-05-01', 'enddate': '2010-05-01'}
-    params = {'datasetid': 'GHCND', 'units': 'metric', 'datatypeid': 'TMAX', 'includemetadata' : False}
-    if bool(m):
-        _,v = m.popitem()
-        params['locationid']= v[0].value['location_id']
-        params['startdate'] = v[0].value['date']
-        params['enddate'] = v[0].value['date']
-        
-        r = requests.get(url, params = params, headers = headers)
-        bar = json.loads(r.text)
-        tmax = {
-            "user_id": v[0].value['user_id'],
-            "job_id": v[0].value['job_id'],
-            "location_id": v[0].value['location_id'],
-            "date": v[0].value['date'],
-            # Number of fields will improve
-            "tmax": bar['results'][0]['value']
+    for message in consumer:
+        params = {
+            'datasetid': 'GHCND',
+            'units': 'metric',
+            'datatypeid': ['TMAX', 'TMIN'],
+            'includemetadata' : False
         }
-        print(tmax)
-        mongosend = copy.deepcopy(tmax)
+
+        weather_req = message.value
+        params['locationid']= weather_req['location_id']
+        params['startdate'] = weather_req['date']
+        params['enddate'] = weather_req['date']
+        
+        response = requests.get(url, params = params, headers = headers)
+        # print(response.text)
+        weather_result = json.loads(response.text)
+        tmax = [field_data for field_data in weather_result['results'] if field_data['datatype'] =='TMAX']
+        tmin = [field_data for field_data in weather_result['results'] if field_data['datatype'] =='TMIN']
+
+        # print(tmax)
+        # print(tmin)
+        
+        weather_details = {
+            "user_id": weather_req['user_id'],
+            "job_id": weather_req['job_id'],
+            "location_id": weather_req['location_id'],
+            "date": weather_req['date'],
+            # Number of fields will improve
+            "tmax": tmax[0]['value'],
+            "tmin": tmin[0]['value']
+        }
+        print(weather_details)
+        mongosend = copy.deepcopy(weather_details)
         result = db.weather.insert_one(mongosend)
         # introduce mock processing time
+        # Todo: Sleep Asynchronously 
         time.sleep(30)
-        # print("From DB")
-        # r = db.weather.find_one({'_id': result.inserted_id })
-        # print(tmax)
-        producer.send('model-execution', value = tmax)
+        producer.send('model-execution', value = weather_details)
