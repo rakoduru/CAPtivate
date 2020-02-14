eaa8b4fcb1f2f62c9abda48705e30c46e37736f9
Adithya Selvaprithiviraj <adithyaselv@gmail.com>
2020-02-13 21:07:12 -0500

    Handle future dates and validate date string in Data retrieval (#66)

--- data-retrieval/data-retrieval.py
+++ data-retrieval/data-retrieval.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 import json
 from kafka import KafkaProducer
 import copy
+from datetime import date
 
 mclient = MongoClient(port=27014)
 
@@ -24,40 +25,67 @@ url ='https://www.ncdc.noaa.gov/cdo-web/api/v2/data'
 
 while True:
     for message in consumer:
-        params = {
-            'datasetid': 'GHCND',
-            'units': 'metric',
-            'datatypeid': ['TMAX', 'TMIN'],
-            'includemetadata' : False
-        }
-
         weather_req = message.value
-        params['locationid']= weather_req['location_id']
-        params['startdate'] = weather_req['date']
-        params['enddate'] = weather_req['date']
-        
-        response = requests.get(url, params = params, headers = headers)
-        # print(response.text)
-        weather_result = json.loads(response.text)
-        tmax = [field_data for field_data in weather_result['results'] if field_data['datatype'] =='TMAX']
-        tmin = [field_data for field_data in weather_result['results'] if field_data['datatype'] =='TMIN']
+        try:
+            params = {
+                'datasetid': 'GHCND',
+                'units': 'metric',
+                'datatypeid': ['TMAX', 'TMIN'],
+                'includemetadata' : False
+            }
 
-        # print(tmax)
-        # print(tmin)
-        
-        weather_details = {
-            "user_id": weather_req['user_id'],
-            "job_id": weather_req['job_id'],
-            "location_id": weather_req['location_id'],
-            "date": weather_req['date'],
-            # Number of fields will improve
-            "tmax": tmax[0]['value'],
-            "tmin": tmin[0]['value']
-        }
-        print(weather_details)
-        mongosend = copy.deepcopy(weather_details)
-        result = db.weather.insert_one(mongosend)
-        # introduce mock processing time
-        # Todo: Sleep Asynchronously 
-        time.sleep(30)
-        producer.send('model-execution', value = weather_details)
+
+            parsed_date = date.fromisoformat(weather_req['date'])
+
+            if parsed_date > date.today() :
+                req_date = parsed_date.replace(year = 2005)
+            else :
+                req_date = parsed_date
+
+            
+            params['locationid']= weather_req['location_id']
+            params['startdate'] = str(req_date)
+            params['enddate'] = str(req_date)
+
+            response = requests.get(url, params = params, headers = headers)
+            # print(response.text)
+            weather_result = json.loads(response.text)
+            tmax = [field_data for field_data in weather_result['results'] if field_data['datatype'] =='TMAX']
+            tmin = [field_data for field_data in weather_result['results'] if field_data['datatype'] =='TMIN']
+
+            # print(tmax)
+            # print(tmin)
+
+            weather_details = {
+                "user_id": weather_req['user_id'],
+                "job_id": weather_req['job_id'],
+                "location_id": weather_req['location_id'],
+                "date": weather_req['date'],
+                "status": weather_req['status'],
+                # Number of fields will improve
+                "tmax": tmax[0]['value'],
+                "tmin": tmin[0]['value']
+            }
+            print(weather_details)
+            mongosend = copy.deepcopy(weather_details)
+            result = db.weather.insert_one(mongosend)
+            # introduce mock processing time
+            # Todo: Sleep Asynchronously 
+            time.sleep(30)
+            producer.send('model-execution', value = weather_details)
+        except Exception as e:
+            # print(e)
+            error_details = {
+                "user_id": weather_req['user_id'],
+                "job_id": weather_req['job_id'],
+                "location_id": weather_req['location_id'],
+                "date": weather_req['date'],
+                "status": "Error",
+                # Number of fields will improve
+                "error": "There was an error processing your query"
+            }
+            print(error_details)
+            mongosend = copy.deepcopy(weather_details)
+            result = db.weather.insert_one(mongosend)
+            producer.send('session-update', value = error_details)
+                    
